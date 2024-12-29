c45ab64330ce23fa5a72171b8975f2b8c7a3dad9
Javier Martínez <143547061+Javierjmc@users.noreply.github.com>
2024-12-29 04:31:35 +0000

    mejorando filtros

--- src/components/ItemsList.jsx
+++ src/components/ItemsList.jsx
@@ -5,9 +5,9 @@ const ItemsList = ({nombre, elo, index, id}) => {
     <li className="text-sm hover:font-semibold sm:text-base grid grid-cols-7 px-4 py-2 hover:bg-blue-200/50 transition hover:translate-y-0.5 ">  
       <div>{index}</div>
       <a href={`https://ratings.fide.com/profile/${id}`} target="_blank" className="col-span-3 hover:underline">{nombre}</a>
-      <div>{elo[0]}</div>
-      <div>{elo[1]}</div>
-      <div>{elo[2]}</div>              
+      <div>{ elo[0] == 0 ? "UR" : elo[0] }</div>
+      <div>{ elo[1] == 0 ? "UR" : elo[1] }</div>
+      <div>{ elo[2] == 0 ? "UR" : elo[2] }</div>              
     </li>
   )
 }
